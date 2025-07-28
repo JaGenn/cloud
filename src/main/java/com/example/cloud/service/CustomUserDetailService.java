ede86b7cab16e3db3c10282321132c6b4d426dfe
JaGenn <dzhan.dzhan.2000@mail.ru>
2025-07-28 17:32:55 +0300

    feat: spring security config

---
+++ src/main/java/com/example/cloud/service/CustomUserDetailService.java
@@ -0,0 +1,29 @@
+package com.example.cloud.service;
+
+import com.example.cloud.model.entity.User;
+import com.example.cloud.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
