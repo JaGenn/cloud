ce262b29919b48f0284477e20dd8bc59fe1aade3
JaGenn <dzhan.dzhan.2000@mail.ru>
2025-08-22 23:00:58 +0300

    fix:

--- src/main/java/com/example/cloud/model/entity/User.java
+++ src/main/java/com/example/cloud/model/entity/User.java
@@ -3,13 +3,15 @@ package com.example.cloud.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 @RequiredArgsConstructor
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/example/cloud/service/CustomUserDetailService.java
+++ src/main/java/com/example/cloud/service/CustomUserDetailService.java
@@ -4,7 +4,6 @@ import com.example.cloud.model.entity.User;
 import com.example.cloud.model.props.CustomUserDetails;
 import com.example.cloud.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -16,15 +15,11 @@ public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) {
+    public CustomUserDetails loadUserByUsername(String username) {
 
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        return new CustomUserDetails(
-                user.getId(),
-                user.getUsername(),
-                user.getPassword()
-        );
+        return new CustomUserDetails(user);
     }
 }
